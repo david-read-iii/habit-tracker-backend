541018f711e1c6a0bacbe0399255fc5c4e468f0e
David Read <david-read-iii@users.noreply.github.com>
2025-07-01 06:47:48 -0400

    Create user model

---
+++ model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    timezone: {
+        type: String,
+        required: true,
+        default: "UTC"
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("User", userSchema);
